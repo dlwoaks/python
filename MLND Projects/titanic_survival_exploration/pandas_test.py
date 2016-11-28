7bd69b3ba330ef25e9f8ad37a66ff615242ff316
Jonathan Lee <dlwoaks@gmail.com>
2016-11-28 18:51:53 -0500

    pandas fillna test

--- MLND Projects/titanic_survival_exploration/pandas_test.py
+++ MLND Projects/titanic_survival_exploration/pandas_test.py
@@ -1,2 +1,6 @@
-import pandas
-titanic = pandas.read_csv("titanic_data.csv")
+import pandas as pd
+titanic = pd.read_csv("titanic_data.csv")
+print(titanic.head(5))
+print(titanic.describe())
+titanic["Age"] = titanic["Age"].fillna(titanic["Age"].median())
+print(titanic.describe())
